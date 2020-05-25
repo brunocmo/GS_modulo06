d0825de93034841f27cef59be2ff9be9420a8f9b
Bruno Nunes <brunocmo.nunes@gmail.com>
2020-05-25 19:10:32 -0300

    Style repository

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronsLeft, FiChevronRight } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo_github.svg';
+
+import { Header, RepositoryInfo, Issues } from './styles';
 
 interface RespositoryParams {
   repository: string;
@@ -8,7 +13,55 @@ interface RespositoryParams {
 const Repository: React.FC = () => {
   const { params } = useRouteMatch<RespositoryParams>();
 
-  return <h1>Repository:{params.repository}</h1>;
+  return (
+    <>
+      <Header>
+        <img src={logoImg} alt="Github Explorer" />
+        <Link to="/">
+          <FiChevronsLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img
+            src="https://avatars3.githubusercontent.com/u/43324383?s=460&u=8bf9fb89a5d27a8e90c5d4355459425d07a1a790&v=4"
+            alt="Rocketseat"
+          />
+          <div>
+            <strong>teste</strong>
+            <p>descricao</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>1808</strong>
+            <span>Starts</span>
+          </li>
+          <li>
+            <strong>1808</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>1808</strong>
+            <span>Issues abertas</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+        <Link to="fuck">
+          <div>
+            <strong>asdf</strong>
+            <p>asdf</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
+  );
 };
 
 export default Repository;
